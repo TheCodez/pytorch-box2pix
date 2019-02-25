2c7ba4b68df68bb4d95149706a1d712d2d6588b7
Michael Kösel <michaelkoesel@gmx.de>
2019-02-26 00:18:26 +0100

    update cityscapes dataset to torchvision master

--- datasets/cityscapes.py
+++ datasets/cityscapes.py
@@ -1,5 +1,3 @@
-from collections import namedtuple
-
 import numpy as np
 import torch
 import torchvision.datasets as datasets
@@ -9,47 +7,6 @@ from utils.box_utils import get_bounding_box
 
 
 class CityscapesDataset(datasets.Cityscapes):
-    # Taken from https://github.com/mcordts/cityscapesScripts
-    Label = namedtuple('Label', ['name', 'id', 'trainId', 'category', 'categoryId', 'hasInstances', 'ignoreInEval',
-                                 'color'])
-
-    classes = [
-        Label('unlabeled', 0, 255, 'void', 0, False, True, (0, 0, 0)),
-        Label('ego vehicle', 1, 255, 'void', 0, False, True, (0, 0, 0)),
-        Label('rectification border', 2, 255, 'void', 0, False, True, (0, 0, 0)),
-        Label('out of roi', 3, 255, 'void', 0, False, True, (0, 0, 0)),
-        Label('static', 4, 255, 'void', 0, False, True, (0, 0, 0)),
-        Label('dynamic', 5, 255, 'void', 0, False, True, (111, 74, 0)),
-        Label('ground', 6, 255, 'void', 0, False, True, (81, 0, 81)),
-        Label('road', 7, 0, 'flat', 1, False, False, (128, 64, 128)),
-        Label('sidewalk', 8, 1, 'flat', 1, False, False, (244, 35, 232)),
-        Label('parking', 9, 255, 'flat', 1, False, True, (250, 170, 160)),
-        Label('rail track', 10, 255, 'flat', 1, False, True, (230, 150, 140)),
-        Label('building', 11, 2, 'construction', 2, False, False, (70, 70, 70)),
-        Label('wall', 12, 3, 'construction', 2, False, False, (102, 102, 156)),
-        Label('fence', 13, 4, 'construction', 2, False, False, (190, 153, 153)),
-        Label('guard rail', 14, 255, 'construction', 2, False, True, (180, 165, 180)),
-        Label('bridge', 15, 255, 'construction', 2, False, True, (150, 100, 100)),
-        Label('tunnel', 16, 255, 'construction', 2, False, True, (150, 120, 90)),
-        Label('pole', 17, 5, 'object', 3, False, False, (153, 153, 153)),
-        Label('polegroup', 18, 255, 'object', 3, False, True, (153, 153, 153)),
-        Label('traffic light', 19, 6, 'object', 3, False, False, (250, 170, 30)),
-        Label('traffic sign', 20, 7, 'object', 3, False, False, (220, 220, 0)),
-        Label('vegetation', 21, 8, 'nature', 4, False, False, (107, 142, 35)),
-        Label('terrain', 22, 9, 'nature', 4, False, False, (152, 251, 152)),
-        Label('sky', 23, 10, 'sky', 5, False, False, (70, 130, 180)),
-        Label('person', 24, 11, 'human', 6, True, False, (220, 20, 60)),
-        Label('rider', 25, 12, 'human', 6, True, False, (255, 0, 0)),
-        Label('car', 26, 13, 'vehicle', 7, True, False, (0, 0, 142)),
-        Label('truck', 27, 14, 'vehicle', 7, True, False, (0, 0, 70)),
-        Label('bus', 28, 15, 'vehicle', 7, True, False, (0, 60, 100)),
-        Label('caravan', 29, 255, 'vehicle', 7, True, True, (0, 0, 90)),
-        Label('trailer', 30, 255, 'vehicle', 7, True, True, (0, 0, 110)),
-        Label('train', 31, 16, 'vehicle', 7, True, False, (0, 80, 100)),
-        Label('motorcycle', 32, 17, 'vehicle', 7, True, False, (0, 0, 230)),
-        Label('bicycle', 33, 18, 'vehicle', 7, True, False, (119, 11, 32)),
-        Label('license plate', -1, -1, 'vehicle', 7, False, True, (0, 0, 142)),
-    ]
 
     def __init__(self, root, split='train', joint_transform=None, img_transform=None):
         super(CityscapesDataset, self).__init__(root, split, target_type=['instance', 'polygon'])
@@ -77,7 +34,7 @@ class CityscapesDataset(datasets.Cityscapes):
         instance_copy = instance.copy()
 
         for cls in self.classes:
-            instance_copy[instance == cls.id] = cls.trainId
+            instance_copy[instance == cls.id] = cls.train_id
         instance = Image.fromarray(instance_copy.astype(np.uint8))
 
         return instance
@@ -89,7 +46,7 @@ class CityscapesDataset(datasets.Cityscapes):
         for obj in objects:
             polygons = obj['polygon']
             cls = self.get_class_from_name(obj['label'])
-            if cls and cls.hasInstances:
+            if cls and cls.has_instances:
                 boxes.append(get_bounding_box(polygons))
                 labels.append(cls.id)
 
@@ -111,7 +68,7 @@ class CityscapesDataset(datasets.Cityscapes):
 
     @staticmethod
     def get_instance_classes():
-        return [cls for cls in CityscapesDataset.classes if cls.hasInstances]
+        return [cls for cls in CityscapesDataset.classes if cls.has_instances]
 
     @staticmethod
     def num_instance_classes():
